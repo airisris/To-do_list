aade33b483d5b7602c1fa2e76fc338a7e5d9962a
Airis Alesya <airisalesya0911@gmail.com>
2025-04-30 14:39:11 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     // put backend code before rendering all the html eements
 
     // Connect to Database
@@ -54,6 +55,17 @@
     >
       <div class="card-body">
         <h3 class="card-title mb-3">My Todo List</h3>
+        <?php if(isset($_SESSION["user"])) : ?>
+            <p>Hello, <?= $_SESSION["user"]["name"]; ?></p>
+            <div>
+                <a href="logout.php">Log Out</a>
+            </div>
+        <?php else : ?>
+            <div>
+            <a href="login.php">Login</a>
+            <a href="signup.php">Sign  Up</a>
+            </div>
+        <?php endif; ?>
         <?php foreach ($todos as $index => $todo) { ?>
             <ul class="list-group">
                 <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -62,37 +74,49 @@
                         <input type="hidden" name="todo_id" value="<?php echo $todo["id"]; ?>"/>
                         <input type="hidden" name="todo_completed" value="<?php echo $todo["completed"]; ?>"/>
                         <?php if ($todo["completed"] == 1) { ?>
-                            <button class="btn btn-sm btn-success"><i class="bi bi-check-square"></i></button>
+                            <?php if(isset($_SESSION["user"])) : ?>
+                                <button class="btn btn-sm btn-success"><i class="bi bi-check-square"></i></button>
+                            <?php else : ?>
+                                <button disabled class="btn btn-sm btn-success"><i class="bi bi-check-square"></i></button>
+                            <?php endif ; ?>
                             <span class="ms-2 text-decoration-line-through"><?php echo $todo["label"] ?></span>
                         <?php } else { ?>
+                            <?php if(isset($_SESSION["user"])) : ?>
                                 <button class="btn btn-sm btn-light"><i class="bi bi-square"></i></button>
-                                <span><?php echo $todo["label"] ?></span>
-                            <?php } ?>
+                            <?php else : ?>
+                                <button disabled class="btn btn-sm btn-light"><i class="bi bi-square"></i></button>
+                            <?php endif ; ?>
+                            <span><?php echo $todo["label"] ?></span>
+                        <?php } ?>
                     </form>
                 </div>
                 <div>
-                    <form method="POST" action="delete_label.php">
-                        <input type="hidden" name="todo_id" value="<?php echo $todo["id"]; ?>"/>
-                        <button class="btn btn-sm btn-danger">
-                            <i class="bi bi-trash"></i>
-                        </button>
-                    </form>
+                    <?php if(isset($_SESSION["user"])) : ?>
+                        <form method="POST" action="delete_label.php">
+                            <input type="hidden" name="todo_id" value="<?php echo $todo["id"]; ?>"/>
+                            <button class="btn btn-sm btn-danger">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                        </form>
+                    <?php endif ; ?>
                 </div>
                 </li>
             </ul>
         <?php } ?>
 
         <div class="mt-4">
-          <form method="POST" action="add_label.php" class="d-flex justify-content-between align-items-center">
-                <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Add new item..."
-                    name="todos_label"
-                    required
-                />
-                <button class="btn btn-primary btn-sm rounded ms-2">Add</button>
-          </form>
+            <?php if(isset($_SESSION["user"])) : ?>
+                <form method="POST" action="add_label.php" class="d-flex justify-content-between align-items-center">
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Add new item..."
+                        name="todos_label"
+                        required
+                    />
+                    <button class="btn btn-primary btn-sm rounded ms-2">Add</button>
+                </form>
+            <?php endif ; ?>
         </div>
       </div>
     </div>
